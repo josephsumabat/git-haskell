665dce1996248148478916d22cf1df7b1acd74e7
Joseph Sumabat <josephrsumabat@gmail.com>
2020-05-03 01:18:19 -0400

    Add helper for partial exception functions

--- src/DefinedExceptions.hs
+++ src/DefinedExceptions.hs
@@ -1,14 +1,21 @@
 module DefinedExceptions
   (
-      ObjException(..)
+      maybeExceptionHelper
+    , ObjException(..)
     , DirException(..)
   ) where
 
 import Control.Exception
 
-data ObjException = ParseException deriving Show
+data ObjException = ParseException | UnexpectedObjectException deriving Show
 
 data DirException = DirNotFoundException deriving Show
 
 instance Exception ObjException
 instance Exception DirException
+
+maybeExceptionHelper::Exception e => (a -> Maybe b) -> e -> a -> b
+maybeExceptionHelper fn exception input = 
+  case (fn input) of
+    Nothing -> throw exception
+    Just x -> x
